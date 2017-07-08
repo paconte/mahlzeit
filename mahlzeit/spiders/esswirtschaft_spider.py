4f710ccb5c22ca25e6397619dd23130f8fc07c5d
paconte <paconte@gmail.com>
2017-07-08 17:27:41 +0200

    Improve scraping from esswirtschaft.
    Extract second lunch of week day and extract vegan and vegetarian plates.

--- mahlzeit/spiders/esswirtschaft_spider.py
+++ mahlzeit/spiders/esswirtschaft_spider.py
@@ -6,49 +6,90 @@ from mahlzeit.items import create_dish_for_week
 from mahlzeit.items import get_monday_date
 from mahlzeit.items import days
 
+
+def extract_ingredients(dish):
+    ingredients = list()
+    dish_aux = dish
+    if '**' in dish:
+        ingredients.append('vegan')
+        ingredients.append('vegetarian')
+        dish_aux = dish.replace('*', '')
+    elif '*' in dish:
+        ingredients.append('vegetarian')
+        dish_aux = dish.replace('*', '')
+    return dish_aux, ingredients
+
+
 class EWSpider(scrapy.Spider):
     name = "esswirtschaft"
     start_urls = ['http://www.esswirtschaft.de/wochenkarte/wochenkarte.html']
     business = 'esswirtschaft'
 
+
+    def create_item(self, dish, date):
+        dish_aux, ingredients = extract_ingredients(dish)
+        return MenuItem(dish=dish_aux, date=date, ingredients=ingredients, business=self.business)
+
+
     def parse(self, response):
         result = list()
         # wochengericht
         wochengericht = response.selector.xpath('//h2[1]//following-sibling::p[1]//text()').extract_first()
-        result.extend(create_dish_for_week(self.business, wochengericht, get_monday_date()))
+        wochengericht, ingredients = extract_ingredients(wochengericht)
+        result.extend(create_dish_for_week(self.business, wochengericht, get_monday_date(), ingredients))
         # wochensalat
         wochensalat = response.selector.xpath('//h2[2]//following-sibling::p[1]//text()').extract_first()
-        result.extend(create_dish_for_week(self.business, wochensalat, get_monday_date()))
+        wochensalat, ingredients = extract_ingredients(wochensalat)
+        result.extend(create_dish_for_week(self.business, wochensalat, get_monday_date(), ingredients))
         # salat im Glas
         salat_im_glas = response.selector.xpath('//h2[3]//following-sibling::p[1]//text()').extract_first()
-        result.extend(create_dish_for_week(self.business, salat_im_glas, get_monday_date()))
+        salat_im_glas, ingredients = extract_ingredients(salat_im_glas)
+        result.extend(create_dish_for_week(self.business, salat_im_glas, get_monday_date(), ingredients))
         # wochensuppen
         wochensuppe1 = response.selector.xpath('//h3//following-sibling::p[1]')[1].extract()[3:-4]
+        wochensuppe1, ingredients = extract_ingredients(wochensuppe1)
         wochensuppe2 = response.selector.xpath('//h3//following-sibling::p[2]//text()').extract_first()
+        wochensuppe2, ingredients = extract_ingredients(wochensuppe2)
         wochensuppe3 = response.selector.xpath('//h3//following-sibling::p[3]//text()').extract_first()
-        result.extend(create_dish_for_week(self.business, wochensuppe1, get_monday_date()))
-        result.extend(create_dish_for_week(self.business, wochensuppe2, get_monday_date()))
-        result.extend(create_dish_for_week(self.business, wochensuppe3, get_monday_date()))
+        wochensuppe3, ingredients = extract_ingredients(wochensuppe3)
+        result.extend(create_dish_for_week(self.business, wochensuppe1, get_monday_date(), ingredients))
+        result.extend(create_dish_for_week(self.business, wochensuppe2, get_monday_date(), ingredients))
+        result.extend(create_dish_for_week(self.business, wochensuppe3, get_monday_date(), ingredients))
 
         # montag
         monday = response.selector.xpath('//h2[4]//following-sibling::p[1]//text()').extract_first()
-        item = MenuItem(dish=monday, date=get_date_of_weekday('montag'), business=self.business)
+        item = self.create_item(monday, get_date_of_weekday('montag'))
+        result.append(item)
+        monday = response.selector.xpath('//h2[4]//following-sibling::p[2]//text()').extract_first()
+        item = self.create_item(monday, get_date_of_weekday('montag'))
         result.append(item)
         # dienstag
         tuesday = response.selector.xpath('//h2[5]//following-sibling::p[1]//text()').extract_first()
-        item = MenuItem(dish=tuesday, date=get_date_of_weekday('dienstag'), business=self.business)
+        item = self.create_item(tuesday, get_date_of_weekday('dienstag'))
+        result.append(item)
+        tuesday = response.selector.xpath('//h2[5]//following-sibling::p[2]//text()').extract_first()
+        item = self.create_item(tuesday, get_date_of_weekday('dienstag'))
         result.append(item)
         # mittwoch
         wednesday = response.selector.xpath('//h2[6]//following-sibling::p[1]//text()').extract_first()
-        item = MenuItem(dish=wednesday, date=get_date_of_weekday('mittwoch'), business=self.business)
+        item = self.create_item(wednesday, get_date_of_weekday('mittwoch'))
+        result.append(item)
+        wednesday = response.selector.xpath('//h2[6]//following-sibling::p[2]//text()').extract_first()
+        item = self.create_item(wednesday, get_date_of_weekday('mittwoch'))
         result.append(item)
         # donnerstag
         thursday = response.selector.xpath('//h2[7]//following-sibling::p[1]//text()').extract_first()
-        item = MenuItem(dish=thursday, date=get_date_of_weekday('donnerstag'), business=self.business)
+        item = self.create_item(thursday, get_date_of_weekday('donnerstag'))
+        result.append(item)
+        thursday = response.selector.xpath('//h2[7]//following-sibling::p[2]//text()').extract_first()
+        item = self.create_item(thursday, get_date_of_weekday('donnerstag'))
         result.append(item)
         # freitag
         friday = response.selector.xpath('//h2[8]//following-sibling::p[1]//text()').extract_first()
-        item = MenuItem(dish=friday, date=get_date_of_weekday('freitag'), business=self.business)
+        item = self.create_item(friday, get_date_of_weekday('freitag'))
+        result.append(item)
+        friday = response.selector.xpath('//h2[8]//following-sibling::p[2]//text()').extract_first()
+        item = self.create_item(friday, get_date_of_weekday('freitag'))
         result.append(item)
 
         return result
