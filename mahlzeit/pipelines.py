99350ba80f4928a9f23604c71b475251a52b2ac5
paconte <paconte@gmail.com>
2017-07-08 17:30:29 +0200

    Make piplelines.py to export extracted items to a target directory.

--- mahlzeit/pipelines.py
+++ mahlzeit/pipelines.py
@@ -4,14 +4,31 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import csv
 from scrapy.exporters import JsonItemExporter
 from scrapy.exporters import CsvItemExporter
 from datetime import datetime
 
 
+pipeline_directory = './exports/'
+
+
+def get_filename(type):
+    date = datetime.today()
+    if type == 'csv':
+        result = pipeline_directory +  'mahlzeit-%s.csv' % date.strftime("%Y-%m-%d")
+    elif type == 'json':
+        result = pipeline_directory +  'mahlzeit-%s.json' % date.strftime("%Y-%m-%d")
+    else:
+        raise AttributeError('Wrong argument %s' % type)
+    return result
+
+
 def clean_item_dish(dish):
     result = dish.replace('\"', '')
     result = dish.replace('"', '')
+    result = result.replace('"', '')
+    result = result.replace(u'\xa0', u'')
     if result[0] == ' ':
         result = result[1:]
     elif result[-1] == ' ':
@@ -19,10 +36,16 @@ def clean_item_dish(dish):
     return result
 
 
+def clean_item_price(price):
+    if price:
+        result = price.replace(' ', '')
+        return result.replace('€', '')
+    return price
+
+
 class JsonExportPipeline(object):
     def __init__(self):
-        date = datetime.today()
-        filename = 'mahlzeit-%s.json' % date.strftime("%Y-%m-%d")
+        filename = get_filename('json')
         self.file = open(filename, 'ab')
         self.exporter = JsonItemExporter(self.file, encoding='utf-8', ensure_ascii=False)
         self.exporter.start_exporting()
@@ -33,16 +56,17 @@ class JsonExportPipeline(object):
 
     def process_item(self, item, spider):
         item['dish'] = clean_item_dish(item['dish'])
+        if 'price' in item:
+            item['price'] = clean_item_price(item['price'])
         self.exporter.export_item(item)
         return item
 
 
 class CsvExportPipeline(object):
     def __init__(self):
-        date = datetime.today()
-        filename = 'mahlzeit-%s.csv' % date.strftime("%Y-%m-%d")
+        filename = get_filename('csv')
         self.file = open(filename, 'ab')
-        self.exporter = CsvItemExporter(self.file, encoding='utf-8', join_multivalued=';')
+        self.exporter = CsvItemExporter(self.file, encoding='utf-8', join_multivalued=';', quoting=csv.QUOTE_ALL)
         self.exporter.start_exporting()
 
     def close_spider(self, spider):
@@ -51,5 +75,7 @@ class CsvExportPipeline(object):
 
     def process_item(self, item, spider):
         item['dish'] = clean_item_dish(item['dish'])
+        if 'price' in item:
+            item['price'] = clean_item_price(item['price'])
         self.exporter.export_item(item)
         return item
