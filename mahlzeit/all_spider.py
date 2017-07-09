a20c820cbfa5b082781c2e5e8bfce518ec59a41f
paconte <paconte@gmail.com>
2017-07-09 14:23:17 +0200

    Use new spider jouisnour.

--- mahlzeit/all_spider.py
+++ mahlzeit/all_spider.py
@@ -4,6 +4,7 @@ from scrapy.utils.project import get_project_settings
 from mahlzeit.spiders.albert_spider import AlbertSpider
 from mahlzeit.spiders.esswirtschaft_spider import EWSpider
 from mahlzeit.spiders.sonnenschein_spider import SonnenscheinSpider
+from mahlzeit.spiders.jouisnour_spider import JouisNourSpider
 #from mahlzeit.spiders.lapetite_spider import LaPetiteSpider
 from mahlzeit.spiders.suppencult_spider import SuppenCultSpider
 
@@ -12,6 +13,7 @@ process = CrawlerProcess(get_project_settings())
 
 process.crawl(AlbertSpider)
 process.crawl(EWSpider)
+process.crawl(JouisNourSpider)
 process.crawl(SuppenCultSpider)
 process.crawl(SonnenscheinSpider)
 
