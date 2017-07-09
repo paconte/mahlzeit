be91691d506b507591f111bb941ec6ad2e99cf2a
paconte <paconte@gmail.com>
2017-07-09 14:24:47 +0200

    Add parsing when price is at the end of the lunch text line.

--- mahlzeit/spiders/sonnenschein_spider.py
+++ mahlzeit/spiders/sonnenschein_spider.py
@@ -16,7 +16,30 @@ def _clean_dish_string(string):
     string = re.sub(exp, '', string)
     exp = re.compile(r'^(– )?')
     string = re.sub(exp, '', string)
+    exp = re.compile(r'^( –)?')
+    string = re.sub(exp, '', string)
     return string
+
+
+def extract_price_at_end(text):
+    price = re.search(r' [0-9]{1}.*€', text)
+    dish = text
+    if price:
+        price = price.group(0)[1:-1].replace(',','.')
+        dish = re.sub(r' [0-9]{1}.*€', '', text)
+    return dish, price
+
+
+def extract_ingredients(dishes):
+    result = list()
+    for text in dishes:
+        subresult = list()
+        exp1 = re.compile(r'\(vegetarisch\)|vegetarisch')
+        found = re.search(exp1, text)
+        if found:
+            subresult.append('vegetarian')
+        result.append(subresult)
+    return result
 
 
 def _parse_weekday(location, business, text, idx1, idx2, weekday):
@@ -24,31 +47,42 @@ def _parse_weekday(location, business, text, idx1, idx2, weekday):
     exp1 = re.compile(r'^Tagessuppe: ')
     exp2 = re.compile(r'^[0-9]{1}(\.)?[ – |– ]')
     exp3 = re.compile(r'^[a-zA-Z]+')
-    price_exp = re.compile(r'^[0-9]{1}.*€')
+    price_exp1 = re.compile(r'^[0-9]{1}.*€')
+
     prices = list()
     dishes = list()
     is_last_dish = False
     for i in range(idx1, idx2):
+        prince_at_end = re.search(r' [0-9]{1}.*€', text[i])
         if exp1.match(text[i]):
             is_last_dish = True
             dish = re.sub(exp1, '', text[i])[:-1]
             dishes.append(dish)
         elif exp2.match(text[i]):
             is_last_dish = True
+            text[i], price = extract_price_at_end(text[i])
             dish = _clean_dish_string(text[i])
             dishes.append(dish)
+            if price:
+                prices.append(price)
         elif exp3.match(text[i]) and is_last_dish:
             is_last_dish = True
             dishes[-1] = dishes[-1] + ' ' + text[i][:-1]
-        elif price_exp.match(text[i]):
+        elif price_exp1.match(text[i]):
             is_last_dish = False
             price = text[i][:-2].replace(',','.')
             prices.append(price)
     if len(prices) != len(dishes):
+        print(is_last_dish)
+        for d in dishes:
+            print(d)
+        for p in prices:
+            print(p)
         raise KeyError('prices and dishes have different length')
+    ingredients = extract_ingredients(dishes)
     result = list()
-    for d, p in zip(dishes, prices):
-        item = MenuItem(location=location ,business=business, dish=d, price=p, date=date)
+    for d, p, i in zip(dishes, prices, ingredients):
+        item = MenuItem(location=location ,business=business, dish=d, price=p, date=date, ingredients=i)
         result.append(item)
     return result
 
@@ -64,6 +98,7 @@ class SonnenscheinSpider(scrapy.Spider):
         return idx
 
     def parse(self, response):
+        items = list()
         filename = create_filename_week('sonnenschein')
         download_and_convert_to_text(response, filename)
         days = ['Montag\n', 'Dienstag\n', 'Mittwoch\n', 'Donnerstag\n', 'Freitag\n']
@@ -75,5 +110,6 @@ class SonnenscheinSpider(scrapy.Spider):
                 except IndexError:
                     idx2 = len(text)
                 idx1 = text.index(days[i])
-                items = _parse_weekday(self.location, self.business, text, idx1, idx2, days[i][:-1])
-            return items
+                print(idx1, idx2, days[i][:-1])
+                items += _parse_weekday(self.location, self.business, text, idx1, idx2, days[i][:-1])
+        return items
