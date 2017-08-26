e848ed2d4a83f580af70b6ff30052f7d35018a93
paconte <paconte@gmail.com>
2017-08-26 16:59:33 +0200

    Move mongodb logic from pipelines.py to db_utils.py

--- mahlzeit/db_utils.py
+++ mahlzeit/db_utils.py
@@ -1,14 +1,31 @@
-from pymongo import MongoClient
+import pymongo
 from bson.json_util import dumps
+from scrapy.conf import settings
 from mahlzeit.date_utils import get_today_midnight
 from mahlzeit.date_utils import get_date_of_weekday
+from mahlzeit.date_utils import get_current_day_week_number
+from mahlzeit.date_utils import get_current_weekday_number
 
 
-client = MongoClient('localhost', 27017)
+client = pymongo.MongoClient('localhost', 27017)
+connection = pymongo.MongoClient(settings['MONGODB_SERVER'], settings['MONGODB_PORT'])
+db = connection[settings['MONGODB_DB']]
+collection = db[settings['MONGODB_COLLECTION']]
 
 
-def count_lunches(list_business=None, start=None):
-    lunches = get_week_lunches_mongodb(list_business, start)
+def print_cursor_to_file(cursor, dst, variable=False):
+    obj = dumps(cursor)
+    with open(dst, "w") as fp:
+        if variable:
+            fp.write('const productsAux = \'')
+            fp.write(obj)
+            fp.write('\';\nconst products = JSON.parse(productsAux);\n')
+            fp.write('export default products;\n')
+        else:
+            fp.write(obj)
+
+
+def count_lunches(lunches, list_business=None, weekday_start=None):
     if isinstance(lunches, dict):
         result = dict()
         for key, cursor in lunches.items():
@@ -18,10 +35,9 @@ def count_lunches(list_business=None, start=None):
     return result
 
 
-def get_week_lunches_mongodb(list_business=None, start=None):
-    collection = client.coolinarius.lunch
+def get_current_week_lunches_mongodb(list_business=None, weekday_start=None):
     # calculate start and finish date
-    if not start:
+    if not weekday_start:
         start_date = get_today_midnight()
     else:
         start_date = get_date_of_weekday('monday')
@@ -37,16 +53,30 @@ def get_week_lunches_mongodb(list_business=None, start=None):
     return result
 
 
-def print_cursor_to_file(cursor, dst, variable=False):
-    obj = dumps(cursor)
-    with open(dst, "w") as fp:
-        if variable:
-            fp.write('const productsAux = \'')
-            fp.write(obj)
-            fp.write('\';\nconst products = JSON.parse(productsAux);\n')
-            fp.write('export default products;\n')
-        else:
-            fp.write(obj)
+def insert_mongodb(item):
+    list_business = list()
+    list_business.append(item['business'])
+    records = get_current_week_lunches_mongodb(list_business)
+    counts = count_lunches(records, list_business)
+    current_week = get_current_day_week_number()
+
+    # item is from current week and db has already items for this week
+    if counts[item['business']] > 0 and get_current_weekday_number() > 4:
+        for record in records:
+            if record['dish'] != item['dish']:
+                # log
+                print('WRONG WEEK???')
+                pass
+    # item is in a future week
+    elif item['date'].isocalendar()[1] > current_week:
+        print('NEXT WEEK')
+        pass
+    else:
+        try:
+            collection.insert(dict(item))
+        except pymongo.errors.DuplicateKeyError:
+            pass
+
 
 
 # db.lunch.createIndex( { date: 1, location: 1, business: 1 , dish: 1}, { unique: true } )
--- mahlzeit/pipelines.py
+++ mahlzeit/pipelines.py
@@ -5,13 +5,12 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import csv
-import pymongo
-from pymongo.errors import DuplicateKeyError
 from datetime import datetime
 from scrapy.exporters import JsonItemExporter
 from scrapy.exporters import CsvItemExporter
 from scrapy.exceptions import DropItem
 from scrapy.conf import settings
+from mahlzeit.db_utils import insert_mongodb
 
 
 def get_filename(file_type):
@@ -99,15 +98,8 @@ class CsvExportPipeline(object):
 
 
 class MongoDBPipeline(object):
-    def __init__(self):
-        connection = pymongo.MongoClient(settings['MONGODB_SERVER'], settings['MONGODB_PORT'])
-        db = connection[settings['MONGODB_DB']]
-        self.collection = db[settings['MONGODB_COLLECTION']]
 
     def process_item(self, item, spider):
-        try:
-            self.collection.insert(dict(item))
-        except DuplicateKeyError:
-            pass
+        insert_mongodb(item)
         #log.msg("Question added to MongoDB database!", level=log.DEBUG, spider=spider)
         return item
