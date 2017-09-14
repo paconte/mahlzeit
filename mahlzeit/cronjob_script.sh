78d65cb04c6d962be122f4f98e6ca1b7436738ad
paconte <paconte@gmail.com>
2017-09-14 20:58:19 +0200

    Change path of cronjob user because before was using an old npm version.

--- mahlzeit/cronjob_script.sh
+++ mahlzeit/cronjob_script.sh
@@ -19,10 +19,11 @@ DEPLOY_SCRIPT=/home/frevilla/devel/culinarius/prepare_deploy_script.sh
 
 
 echo "######### START RUNNING SCRIPT $(date) ##########" >> /tmp/mybackup.log
-PATH=$PATH:/usr/sbin:/usr/bin:/sbin:/bin:/home/frevilla/.local/bin/scrapy
+#PATH=$PATH:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/home/frevilla/.local/bin/scrapy
+PATH=/home/frevilla/bin:/home/frevilla/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
 export PATH
 cd /home/frevilla/devel/scrapy/mahlzeit/
-/home/frevilla/.local/bin/scrapy coolinarius --backup --run --frontend --deploy
+/home/frevilla/.local/bin/scrapy coolinarius --backup --run --force-deploy
 print_command_output
 echo "######### STOP RUNNING SCRIPT $(date) ##########" >> /tmp/mybackup.log
 
